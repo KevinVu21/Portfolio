635a187821b843005d81092a967125593c0560dd
Kevin21 <kevu4776@gmail.com>
2025-01-08 01:29:50 -0600

    added header

---
+++ src/header.jsx
@@ -0,0 +1,16 @@
+import './header.css';
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='left'>Kevu</div>
+      <div className='right'>
+        <div>About</div>
+        <div>Experience</div>
+        <div>Hobbies</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
